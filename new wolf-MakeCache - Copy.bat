fe5f11652b49ed1ad72cf92e824500db60f042aa
thewolfgames <thewolfgames@yahoo.com>
2021-05-27 23:29:40 +0300

    75X

--- new wolf-MakeCache - Copy.bat
+++ new wolf-MakeCache - Copy.bat
@@ -1,16 +1,12 @@
 @echo off
-
 echo CACHE MANIFEST > test0.txt
+echo # %date%-%time% >> test0.txt
 echo. >> test0.txt
-echo CACHE: >> test0.txt
-
 set LOC=%~dp0
 
 dir /B /S /A:-D >> test0.txt
 
 echo. >> test0.txt
-echo NETWORK: >> test0.txt
-echo * >> test0.txt
 findstr /v "media .bat .exe .mp4 .git .py restore New folder wolf.manifest test0.txt" test0.txt > test.txt
 del test0.txt
 
