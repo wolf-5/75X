ae0a862bf52921790586adc9a7f1216af92b753e
thewolfgames <thewolfgames@yahoo.com>
2021-06-14 19:29:04 +0300

    75X

--- MakeCache.bat
+++ MakeCache.bat
@@ -1,13 +1,13 @@
 @echo off
 echo CACHE MANIFEST > test0.txt
-echo # %date%-%time% >> test0.txt
+echo # v2.5.5 Self-Host >> test0.txt
 echo. >> test0.txt
+
 set LOC=%~dp0
 
 dir /B /S /A:-D >> test0.txt
 
-echo. >> test0.txt
-findstr /v "media .bat .exe .mp4 .git .py restore New folder offlineCache.manifest test0.txt" test0.txt > test.txt
+findstr /v "media .bat .exe .mp4 New folder .git restore offlinexmb.cache test0.txt" test0.txt > test.txt
 del test0.txt
 
 @echo off
@@ -27,9 +27,9 @@ set "firstLineReady="
 if defined firstLineReady (echo()
 set "firstLineReady=1"
 <nul set /p "=%%a")
-) > offlineCache.manifest
+) > offlinexmb.cache
 del test.txt
 
-echo offlineCache.manifest created!!
+echo offlinexmb.cache created!!
 
 sleep 2
